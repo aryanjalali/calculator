0ecc62f116a3cb3c4a3fdee98e33ddd5c0ed5d82
aryanjalali <aryanjalalicoding@gmail.com>
2023-08-06 16:19:12 -0400

    Create boilerplate files and HTML outline
    Created basic HTML, CSS and JS files, and linked them all together. Also
    created basic outline of HTML sections and gave them ids/classes that I
    expect to use for CSS and JS

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+Pseudocode ?
+
+Take user number input(s)
+Take user sign input
+Store numbers up to now as current number
+Store sign as current math function
+Take user number input(s)
+User clicks enter signals to apply math function to two numbers
+OR user clicks another math function signals to perform calculation
+Make result the new current number and display it
+
+*/
